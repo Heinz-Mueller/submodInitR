85fb4236da41514d4d3c9db242681f70340f1f13
Fuse <noobomat2@gmx.de>
2018-09-19 00:53:10 +0200

    commit im sub

---
+++ sub-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-sub-comp',
+  templateUrl: './sub-comp.component.html',
+  styleUrls: ['./sub-comp.component.scss']
+})
+export class SubCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
